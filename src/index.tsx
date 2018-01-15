f883a9c24e7ff2f46c8a8b7657f988810ab2ebad
Rodrigo Landaeta <rlandaeta@cochlear.com>
2018-01-15 11:15:37 +1100

    Added typescript proj def

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { AppRegistry, View, Text, Animated } from "react-native";
+
+class App extends React.PureComponent {
+    public render() {
+        return (
+            <View>
+            </View>
+        );
+    }
+}
+
+AppRegistry.registerComponent('animateflex', () => App);
