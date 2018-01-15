fde7bd786b1f986523118d633643eda003b1703b
Rodrigo Landaeta <rlandaeta@cochlear.com>
2018-01-15 12:30:59 +1100

    Added vs launcher and created the index to be able to display something on the view

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,55 @@
 import * as React from "react";
-import { AppRegistry, View, Text, Animated } from "react-native";
+import { AppRegistry, View, Text, Animated, StyleSheet } from "react-native";
+
+enum Colors {
+    Foreground = "white",
+    FirstBox = "#5c97ea",
+    FirstBoxSeparator = "#548ad5",
+    SecondBox = "#fdac2b",
+    SecondBoxSeparator = "#e69d27",
+    ThirdBox = "#5cb0ad",
+    ThirdBoxSeparator = "#4c918e",
+}
+
+const styles = StyleSheet.create({
+    container: { flex: 1, padding: 30, alignContent: "center", justifyContent: "center" },
+    title: { fontWeight: "bold", fontSize: 24, color: Colors.Foreground },
+    box: { minHeight: 100 },
+    sectionBox: { flexGrow: 1, minHeight: 100 },
+    topSectionBox: { borderTopLeftRadius: 8, borderTopRightRadius: 8 },
+    bottomSectionBox: { borderBottomLeftRadius: 8, borderBottomRightRadius: 8 },
+    firstBox: { backgroundColor: Colors.FirstBoxSeparator },
+    secondBox: { backgroundColor: Colors.SecondBoxSeparator },
+    thirdBox: { backgroundColor: Colors.ThirdBoxSeparator },
+    firstInnerBox: { backgroundColor: Colors.FirstBox, marginBottom: 10, padding: 10 },
+    secondInnerBox: { backgroundColor: Colors.SecondBox, marginBottom: 10, padding: 10 },
+    thirdInnerBox: { backgroundColor: Colors.ThirdBox, marginBottom: 10, padding: 10 },
+});
 
 class App extends React.PureComponent {
+
+    constructor(props: any) {
+        super(props);
+    }
+
     public render() {
         return (
-            <View>
+            <View style={styles.container}>
+                <View style={[styles.box, styles.topSectionBox, styles.firstBox]}>
+                    <View style={[styles.sectionBox, styles.topSectionBox, styles.firstInnerBox]}>
+                        <Text style={styles.title}>Section 1</Text>
+                    </View>
+                </View>
+                <View style={[styles.box, styles.secondBox]}>
+                    <View style={[styles.sectionBox, styles.secondInnerBox]}>
+                        <Text style={styles.title}>Section 2</Text>
+                    </View>
+                </View>
+                <View style={[styles.box, styles.bottomSectionBox, styles.thirdBox]}>
+                    <View style={[styles.sectionBox, styles.bottomSectionBox, styles.thirdInnerBox]}>
+                        <Text style={styles.title}>Section 3</Text>
+                    </View>
+                </View>
             </View>
         );
     }
